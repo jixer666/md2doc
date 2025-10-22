a942ebd17e34da55326a7b8649ed9fa633cb0397
lijunxi <2770063826@qq.com>
2025-10-22 23:26:06 +0800

    feat:发邮件

--- md2doc-system/src/main/java/com/abc/system/strategy/email/EmailStrategy.java
+++ md2doc-system/src/main/java/com/abc/system/strategy/email/EmailStrategy.java
@@ -1,9 +1,12 @@
 package com.abc.system.strategy.email;
 
+import cn.hutool.extra.spring.SpringUtil;
+import com.abc.common.exception.GlobalException;
 import com.abc.system.domain.dto.EmailDTO;
 import com.abc.system.domain.vo.EmailVO;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -17,14 +20,20 @@ public interface EmailStrategy {
 
     String getContent(Map<String, String> params);
 
-    default void doSend(JavaMailSender mailSender, EmailDTO emailDTO) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(emailDTO.getFrom());
-        helper.setTo(emailDTO.getTo());
-        helper.setSubject(getTitle(emailDTO.getDetailsMap()));
-        helper.setText(getContent(emailDTO.getDetailsMap()), true);
-        mailSender.send(message);
+    default void doSend(JavaMailSender mailSender, EmailDTO emailDTO) {
+        SpringUtil.getBean(ThreadPoolTaskExecutor.class).submit(() -> {
+            try {
+                MimeMessage message = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message, true);
+                helper.setFrom(emailDTO.getFrom());
+                helper.setTo(emailDTO.getTo());
+                helper.setSubject(getTitle(emailDTO.getDetailsMap()));
+                helper.setText(getContent(emailDTO.getDetailsMap()), true);
+                mailSender.send(message);
+            } catch (MessagingException e) {
+                throw new GlobalException(e.getMessage());
+            }
+        });
     }
 
 }
